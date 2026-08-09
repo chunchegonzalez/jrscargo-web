32d484b910b88b6f1b2a37af46a192451401b83e
chunchegonzalez <chunchegonzalez@gmail.com>
2026-08-08 23:46:05 -0600

    Variables finales

--- data/rates.ts
+++ data/rates.ts
@@ -8,3 +8,7 @@ export const rates = {
 export const WHATSAPP_NUMBER = '50672601238';
 export const EMAIL = 'info@jrscargocr.com';
 export const INSTAGRAM_URL = 'https://www.instagram.com/jrscargocr/';
+export const PORTAL_REGISTER_URL = 'https://worldboxcr.com/jrscargo/register';
+export const PORTAL_LOGIN_URL = 'https://worldboxcr.com/jrscargo/login';
+export const MAPS_URL = 'https://maps.google.com';
+export const RATES = rates;
